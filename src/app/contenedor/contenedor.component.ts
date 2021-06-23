3684d96d6afe8b8ab0583b964767afb2b4c83e93
Lucas <lorozcoe@everis.com>
2021-06-23 17:04:48 -0400

    update

---
+++ src/app/contenedor/contenedor.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-contenedor',
+  templateUrl: './contenedor.component.html',
+  styleUrls: ['./contenedor.component.css']
+})
+export class ContenedorComponent {
+   names: string[] =['PEPE','JUAN','MARIA'];
+   description: string[] =['hombre','hombre','mujer'];
+  
+  
+
+}
